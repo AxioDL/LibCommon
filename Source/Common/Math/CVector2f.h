826724d8a35f92db5bb31f7b097b72a31df379aa
Lioncash <mathew1800@gmail.com>
2020-06-09 05:51:03 -0400

    CVector2f: Make constexpr capable

--- Source/Common/Math/CVector2f.h
+++ Source/Common/Math/CVector2f.h
@@ -3,43 +3,102 @@
 
 #include <Common/FileIO/IInputStream.h>
 #include <Common/FileIO/IOutputStream.h>
+#include <cmath>
 
 class CVector2f
 {
 public:
-    float X, Y;
-    CVector2f();
-    CVector2f(float XY);
-    CVector2f(float _X, float _Y);
-    CVector2f(IInputStream& rInput);
+    float X = 0.0f;
+    float Y = 0.0f;
+
+    constexpr CVector2f() = default;
+    constexpr CVector2f(float XY) : X{XY}, Y{XY} {}
+    constexpr CVector2f(float X_, float Y_) : X{X_}, Y{Y_} {}
+    explicit CVector2f(IInputStream& rInput);
     void Write(IOutputStream& rOutput) const;
 
-    float Magnitude() const;
-    float SquaredMagnitude() const;
-    CVector2f Normalized() const;
-    float Dot(const CVector2f& rkOther) const;
+    [[nodiscard]] float Magnitude() const {
+        return std::sqrt(SquaredMagnitude());
+    }
+    [[nodiscard]] float SquaredMagnitude() const {
+        return Dot(*this);
+    }
+    [[nodiscard]] CVector2f Normalized() const {
+        return *this / Magnitude();
+    }
+    [[nodiscard]] constexpr float Dot(const CVector2f& other) const {
+        return (X * other.X) + (Y * other.Y);
+    }
 
-    CVector2f operator+(const CVector2f& rkOther) const;
-    CVector2f operator-(const CVector2f& rkOther) const;
-    CVector2f operator*(const CVector2f& rkOther) const;
-    CVector2f operator/(const CVector2f& rkOther) const;
-    void operator+=(const CVector2f& rkOther);
-    void operator-=(const CVector2f& rkOther);
-    void operator*=(const CVector2f& rkOther);
-    void operator/=(const CVector2f& rkOther);
-    CVector2f operator+(const float Other) const;
-    CVector2f operator-(const float Other) const;
-    CVector2f operator*(const float Other) const;
-    CVector2f operator/(const float Other) const;
-    void operator+=(const float Other);
-    void operator-=(const float Other);
-    void operator*=(const float Other);
-    void operator/=(const float Other);
-    bool operator==(const CVector2f& rkOther) const;
-    bool operator!=(const CVector2f& rkOther) const;
-    CVector2f operator-() const;
-    float& operator[](long Index);
-    const float& operator[](long Index) const;
+    [[nodiscard]] constexpr CVector2f operator+(const CVector2f& other) const {
+        return {X + other.X, Y + other.Y};
+    }
+    [[nodiscard]] constexpr CVector2f operator-(const CVector2f& other) const {
+        return {X - other.X, Y - other.Y};
+    }
+    [[nodiscard]] constexpr CVector2f operator*(const CVector2f& other) const {
+        return {X * other.X, Y * other.Y};
+    }
+    [[nodiscard]] constexpr CVector2f operator/(const CVector2f& other) const {
+        return {X / other.X, Y / other.Y};
+    }
+
+    constexpr void operator+=(const CVector2f& other) {
+        *this = *this + other;
+    }
+    constexpr void operator-=(const CVector2f& other) {
+        *this = *this - other;
+    }
+    constexpr void operator*=(const CVector2f& other) {
+        *this = *this * other;
+    }
+    constexpr void operator/=(const CVector2f& other) {
+        *this = *this / other;
+    }
+
+    [[nodiscard]] constexpr CVector2f operator+(const float other) const {
+        return {X + other, Y + other};
+    }
+    [[nodiscard]] constexpr CVector2f operator-(const float other) const {
+        return {X - other, Y - other};
+    }
+    [[nodiscard]] constexpr CVector2f operator*(const float other) const {
+        return {X * other, Y * other};
+    }
+    [[nodiscard]] constexpr CVector2f operator/(const float other) const {
+        return {X / other, Y / other};
+    }
+
+    constexpr void operator+=(const float other) {
+        *this = *this + other;
+    }
+    constexpr void operator-=(const float other) {
+        *this = *this - other;
+    }
+    constexpr void operator*=(const float other) {
+        *this = *this * other;
+    }
+    constexpr void operator/=(const float other) {
+        *this = *this / other;
+    }
+
+    [[nodiscard]] constexpr bool operator==(const CVector2f& other) const {
+        return X == other.X && Y == other.Y;
+    }
+    [[nodiscard]] constexpr bool operator!=(const CVector2f& other) const {
+        return !operator==(other);
+    }
+
+    [[nodiscard]] constexpr CVector2f operator-() const {
+        return {-X, -Y};
+    }
+
+    [[nodiscard]] constexpr float& operator[](long Index) {
+        return (&X)[Index];
+    }
+    [[nodiscard]] constexpr const float& operator[](long Index) const {
+        return (&X)[Index];
+    }
 
     // Static Members
     static const CVector2f skZero;
