d4f3080c96b2e43c60b4c4eac71a74bd2d7d21b2
Lioncache <mai.iam2048@gmail.com>
2026-01-13 21:37:37 -0500

    CScopedTimer: Use std::string_view instead of std::string
    std::string is a little heavyweight for a class intended to be used
    through the macro.
    
    Also drops the class down to 64 bytes.

--- Source/Common/CScopedTimer.h
+++ Source/Common/CScopedTimer.h
@@ -3,18 +3,18 @@
 
 #include "Common/CTimer.h"
 #include "Common/Log.h"
-#include <string>
+#include <string_view>
 
 // Runs a timer and automatically stops + prints the time to the log when it goes out of scope.
 class CScopedTimer
 {
     CTimer mTimer;
-    std::string mTimerName;
+    std::string_view mTimerName;
     bool mStopped = false;
 
 public:
-    explicit CScopedTimer(std::string timerName)
-        : mTimerName(std::move(timerName))
+    explicit CScopedTimer(std::string_view timerName)
+        : mTimerName(timerName)
     {
         mTimer.Start();
     }
