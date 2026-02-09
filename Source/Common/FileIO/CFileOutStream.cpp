16a8404347c318407206a82c8f84a6b93999aa37
Lioncache <mai.iam2048@gmail.com>
2026-02-09 08:37:55 -0500

    CFileOutStream: Fix seek status reporting
    fseek/fseeko/_fseeki64 all return 0 to report success.

--- Source/Common/FileIO/CFileOutStream.cpp
+++ Source/Common/FileIO/CFileOutStream.cpp
@@ -82,7 +82,7 @@ bool CFileOutStream::Seek(int32_t Offset, uint32_t Origin)
     if (!IsValid())
         return false;
 
-    return (fseek(mpFStream, Offset, Origin) != 0);
+    return (fseek(mpFStream, Offset, Origin) == 0);
 }
 
 bool CFileOutStream::Seek64(int64_t Offset, uint32_t Origin)
@@ -91,9 +91,9 @@ bool CFileOutStream::Seek64(int64_t Offset, uint32_t Origin)
         return false;
 
 #ifdef _WIN32
-    return (_fseeki64(mpFStream, Offset, Origin) != 0);
+    return (_fseeki64(mpFStream, Offset, Origin) == 0);
 #else
-    return (fseeko(mpFStream, Offset, Origin) != 0);
+    return (fseeko(mpFStream, Offset, Origin) == 0);
 #endif
 }
 
