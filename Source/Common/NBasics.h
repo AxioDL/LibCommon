b1130cdeabee5269ba8f57027e4f13941b72dd54
Lioncache <mai.iam2048@gmail.com>
2026-02-21 16:35:25 -0500

    NBasics: Simplify interface
    Many of these are now present in the standard lib, or can just be
    simplified.

--- Source/Common/NBasics.h
+++ Source/Common/NBasics.h
@@ -1,100 +1,36 @@
 #ifndef AXIO_NBASICS_H
 #define AXIO_NBASICS_H
 
-#include <list>
+#include <algorithm>
 #include <vector>
 
 namespace NBasics
 {
 
 /** Remove an element from a container */
-template<typename T, typename ContainerT>
-bool ContainerRemoveOne(ContainerT& Container, const T& kElement)
+template <typename T, typename ContainerT>
+bool RemoveOne(ContainerT& Container, const T& kElement)
 {
-    for (auto Iter = Container.begin(); Iter != Container.end(); Iter++)
-    {
-        if (*Iter == kElement)
-        {
-            Container.erase(Iter);
-            return true;
-        }
-    }
-    return false;
-}
+    const auto iter = std::ranges::find(Container, kElement);
+    if (iter == std::end(Container))
+        return false;
 
-template<typename T>
-inline bool VectorRemoveOne(std::vector<T>& Vector, const T& kElement)
-{
-    return ContainerRemoveOne< T, std::vector<T> >(Vector, kElement);
-}
-
-template<typename T>
-inline bool ListRemoveOne(std::list<T>& List, const T& kElement)
-{
-    return ContainerRemoveOne< T, std::list<T> >(List, kElement);
-}
-
-/** Remove all occurrences of an element from a container. Returns the number of elements that were removed. */
-template<typename T, typename ContainerT>
-int ContainerRemoveAll(ContainerT& Container, const T& kElement)
-{
-    int NumRemoved = 0;
-
-    for (auto Iter = Container.begin(); Iter != Container.end(); Iter++)
-    {
-        if (*Iter == kElement)
-        {
-            Iter = Container.erase(Iter);
-            NumRemoved++;
-        }
-    }
-
-    return NumRemoved;
-}
-
-template<typename T>
-inline int VectorRemoveAll(std::vector<T>& Vector, const T& kElement)
-{
-    return ContainerRemoveAll< T, std::vector<T> >(Vector, kElement);
-}
-
-template<typename T>
-inline int ListRemoveAll(std::list<T>& List, const T& kElement)
-{
-    return ContainerRemoveAll< T, std::list<T> >(List, kElement);
-}
-
-/** Returns the index of an element in a vector. Returns -1 if the element is not present. */
-template<typename T>
-int VectorFind(std::vector<T>& Vector, const T& kElement)
-{
-    for (int i=0; i<Vector.size(); i++)
-    {
-        if (Vector[i] == kElement)
-        {
-            return i;
-        }
-    }
-
-    return -1;
+    Container.erase(iter);
+    return true;
 }
 
 /** Adds an element to a vector only if it is not already present. Returns the index of the element. */
-template<typename T>
-int VectorAddUnique(std::vector<T>& Vector, const T& kElement)
+template <typename T>
+size_t VectorAddUnique(std::vector<T>& Vector, const T& kElement)
 {
-    for (int i=0; i<Vector.size(); i++)
-    {
-        if (Vector[i] == kElement)
-        {
-            return i;
-        }
-    }
+    const auto iter = std::ranges::find(Vector, kElement);
+    if (iter != std::end(Vector))
+        return size_t(std::distance(std::begin(Vector), iter));
 
     Vector.push_back(kElement);
     return Vector.size() - 1;
 }
 
-}
+} // namespace NBasics
 
 #endif // AXIO_NBASICS_H
